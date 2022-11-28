1621fbc6b9a439f157fe1961f16c2028bd1082e6
Ashley <ashleyshi006@gmail.com>
2022-11-28 11:05:24 -0500

    add sidebar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,127 @@
+import "./sidebar.css";
+import RssFeedIcon from "@mui/icons-material/RssFeed";
+import ChatIcon from "@mui/icons-material/Chat";
+import PlayCircleFilledIcon from "@mui/icons-material/PlayCircleFilled";
+import GroupIcon from "@mui/icons-material/Group";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
+import EventIcon from "@mui/icons-material/Event";
+import SchoolIcon from "@mui/icons-material/School";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeedIcon className="sidebarIcon" />
+            <span className="sidebarListItemTest">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <ChatIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Chats</span>
+          </li>
+          <li className="sidebarListItem">
+            <PlayCircleFilledIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Videos</span>
+          </li>
+          <li className="sidebarListItem">
+            <GroupIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Groups</span>
+          </li>
+          <li className="sidebarListItem">
+            <BookmarkIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Bookmarks</span>
+          </li>
+          <li className="sidebarListItem">
+            <HelpOutlineIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+            <WorkOutlineIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Jobs</span>
+          </li>
+          <li className="sidebarListItem">
+            <EventIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Events</span>
+          </li>
+          <li className="sidebarListItem">
+            <SchoolIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Courses</span>
+          </li>
+        </ul>
+        <button className="sidebarButton">Show More</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/person/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/person/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/person/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/person/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/person/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/person/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/person/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/person/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Doe</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,20 @@
 import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./home.css";
 
 const Home = () => {
-  return <Topbar />;
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
 };
 
 export default Home;
