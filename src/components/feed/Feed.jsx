04f141b15d3efce3be88c01e19e8e14907eb7c18
Ashley <ashleyshi006@gmail.com>
2022-11-28 14:05:48 -0500

    add post and share components

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,7 +1,16 @@
+import Post from "../post/Post";
+import Share from "../share/Share";
 import "./feed.css";
 
 const Feed = () => {
-  return <div className="feed">feed</div>;
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        <Post />
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
---
+++ src/components/post/Post.jsx
@@ -0,0 +1,41 @@
+import "./post.css";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+const Post = () => {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              className="postProfileImg"
+              src="/assets/person/1.jpeg"
+              alt=""
+            />
+            <span className="postUsername">Ashley</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVertIcon />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">Hey! It's my first post:)</span>
+          <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="/assets/like.png" alt="" />
+            <img className="likeIcon" src="/assets/heart.png" alt="" />
+            <span className="postLikeCounter">32 people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
