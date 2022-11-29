785b89a0f1b2d4d989984762a8e9a5f098af8771
Ashley <ashleyshi006@gmail.com>
2022-11-28 22:11:54 -0500

    add register page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import Login from "./pages/login/Login";
-import Profile from "./pages/profile/Profile";
+import Home from "./pages/home/Home";
 
 function App() {
-  return <Login />;
+  return <Home />;
 }
 
 export default App;
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -98,7 +98,7 @@ const Rightbar = ({ profile }) => {
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
-        <ProfileRightbar />
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
   );
