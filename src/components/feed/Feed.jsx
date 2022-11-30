7961bdd34bf34ff10e7be82d1475adde9a4e89e3
Ashley <ashleyshi006@gmail.com>
2022-11-30 11:09:08 -0500

    fetch posts and users api

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,15 +1,25 @@
+import { useState, useEffect } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-import { Posts } from "../../dummyData";
+import axios from "axios";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("posts/timeline/637d2622bad1ef1aacf97390");
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((p) => (
-          <Post key={p.id} post={p} />
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
         ))}
       </div>
     </div>
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,12 +1,22 @@
 import "./post.css";
+import { useState, useEffect } from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { Users } from "../../dummyData";
-import { useState } from "react";
+import axios from "axios";
+import { format } from "timeago.js";
 
 const Post = ({ post }) => {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-  const [like, setLike] = useState(post.like);
+  const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`users/${[post.userId]}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [post.userId]);
 
   const likeHandler = () => {
     setLike(isLiked ? like - 1 : like + 1);
@@ -20,13 +30,11 @@ const Post = ({ post }) => {
           <div className="postTopLeft">
             <img
               className="postProfileImg"
-              src={Users.filter((u) => u.id === post.userId)[0].profilePicture}
+              src={user.profilePicture || PF + "person/noAvatar.png"}
               alt=""
             />
-            <span className="postUsername">
-              {Users.filter((u) => u.id === post.userId)[0].username}
-            </span>
-            <span className="postDate">{post.date}</span>
+            <span className="postUsername">{user.username}</span>
+            <span className="postDate">{format(post.createdAt)}</span>
           </div>
           <div className="postTopRight">
             <MoreVertIcon />
@@ -34,7 +42,7 @@ const Post = ({ post }) => {
         </div>
         <div className="postCenter">
           <span className="postText">{post?.desc}</span>
-          <img className="postImg" src={PF + post.photo} alt="" />
+          <img className="postImg" src={PF + post.img} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
